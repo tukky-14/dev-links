55bf04f03c9394cc8f8cf5533b076c2dd2008b40
tukky-14 <bigoonage0104@gmail.com>
2023-03-05 15:09:08 +0900

    アニメーションの追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            animation: {
+                'fade-in-top': 'fade-in-top 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both',
+            },
+            keyframes: {
+                'fade-in-top': {
+                    '0%': {
+                        transform: 'translateY(-50px)',
+                        opacity: '0',
+                    },
+                    to: {
+                        transform: 'translateY(0)',
+                        opacity: '1',
+                    },
+                },
+            },
+        },
+    },
+    plugins: [],
+};
