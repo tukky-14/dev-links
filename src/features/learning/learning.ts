cd0f8872e6eb9edb38f615b53108963f4185253d
tukky-14 <bigoonage0104@gmail.com>
2023-03-05 21:39:11 +0900

    学習ページを更新

---
+++ src/features/learning/learning.ts
@@ -0,0 +1,99 @@
+import paiza from './img/paiza.png';
+import progate from './img/progate.png';
+import dotinstall from './img/dotinstall.png';
+import atcoder from './img/atcoder.png';
+import udemy from './img/udemy.png';
+import techpit from './img/techpit.png';
+import chotdesign from './img/chotdesign.png';
+import schoo from './img/schoo.png';
+import sharewis from './img/sharewis.png';
+import envader from './img/envader.png';
+import recursion from './img/recursion.png';
+import codestep from './img/codestep.png';
+import algomethod from './img/algomethod.png';
+
+export const learningSites = [
+    {
+        title: 'ドットインストール',
+        url: 'https://dotinstall.com/',
+        image: dotinstall,
+        description: '短い動画で、Web開発やプログラミングの初歩から応用までを学べる。',
+    },
+    {
+        title: 'Progate',
+        url: 'https://prog-8.com/',
+        image: progate,
+        description: 'ブラウザ上でコーディングを体験できる。初心者向けのコースが豊富。',
+    },
+    {
+        title: 'Paiza',
+        url: 'https://paiza.jp/',
+        image: paiza,
+        description:
+            'アルゴリズムの問題の練習ができ、転職に紐付けできる。動画のプログラミング講座も多数で、環境もある。また、ストーリー性のあるゲームも。',
+    },
+    {
+        title: 'Atcoder',
+        url: 'https://atcoder.jp/',
+        image: atcoder,
+        description:
+            'プログラミングコンテストに参加できる。問題に取り組むことで、高度なアルゴリズムを学べる。',
+    },
+    {
+        title: 'Udemy',
+        url: 'https://www.udemy.com/',
+        image: udemy,
+        description: '豊富な講座がある。初心者から上級者まで対象のコースがある。',
+    },
+    {
+        title: 'Techpit',
+        url: 'https://www.techpit.jp/',
+        image: techpit,
+        description: 'Web開発に特化したオンライン講座。サービスを作りながら学べる実践的な内容。',
+    },
+    {
+        title: 'chot.design',
+        url: 'https://chot.design/',
+        image: chotdesign,
+        description:
+            'UI/UXデザインに特化したオンライン講座。様々な分野のデザイナーが講師を務めている。',
+    },
+    {
+        title: 'Schoo',
+        url: 'https://schoo.jp/',
+        image: schoo,
+        description:
+            '1つの講座に複数の講師が登場し、幅広い知識を学べる。IT分野以外にもビジネススキルなども学べる大人の学校。',
+    },
+    {
+        title: 'ShareWis',
+        url: 'https://share-wis.com/',
+        image: sharewis,
+        description: '仕事に役立つスキルを学べる。90秒の無料動画から本格的な講座まで幅広い。',
+    },
+    {
+        title: 'Envader',
+        url: 'https://envader.plus/',
+        image: envader,
+        description:
+            'サーバー構築やネットワーク、サイバーセキュリティなどの 仕組みを 学ぶことができるオンライン学習プラットフォーム。',
+    },
+    {
+        title: 'Recursion',
+        url: 'https://recursionist.io/',
+        image: recursion,
+        description: 'コンピュータサイエンスの基礎力を身につけるためのコンテンツが豊富にある。',
+    },
+    {
+        title: 'Codestep',
+        url: 'https://code-step.com/',
+        image: codestep,
+        description: '作って学ぶコーディング学習サイト。',
+    },
+    {
+        title: 'アルゴ式',
+        url: 'https://algo-method.com/',
+        image: algomethod,
+        description: 'アルゴリズムやデータ構造に特化したオンライン学習プラットフォーム。',
+    },
+];
