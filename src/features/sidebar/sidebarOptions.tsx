79b8e7bf1049a6db47338b704efda331985f43a5
tukky-14 <bigoonage0104@gmail.com>
2023-03-05 22:58:44 +0900

    ビルドがエラーになる問題を修正

--- src/features/sidebar/sidebarOptions.tsx
+++ src/features/sidebar/sidebarOptions.tsx
@@ -1,40 +1,40 @@
-import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
-import LanguageTwoToneIcon from '@mui/icons-material/LanguageTwoTone';
-import SchoolTwoToneIcon from '@mui/icons-material/SchoolTwoTone';
-import ArticleTwoToneIcon from '@mui/icons-material/ArticleTwoTone';
-import LabelTwoToneIcon from '@mui/icons-material/LabelTwoTone';
-import PanoramaTwoToneIcon from '@mui/icons-material/PanoramaTwoTone';
-import MoreHorizTwoToneIcon from '@mui/icons-material/MoreHorizTwoTone';
+import home from './img/home.svg';
+import language from './img/language.svg';
+import learning from './img/learning.svg';
+import article from './img/article.svg';
+import useful from './img/useful.svg';
+import material from './img/material.svg';
+import other from './img/other.svg';
 
 export const SidebarOptions = () => {
     return (
         <>
             <a href="/" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <HomeTwoToneIcon className="h-4" />
+                <img className="h-4" src={home} alt="" />
                 <span className="pl-2">ホーム</span>
             </a>
             <a href="/language" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <LanguageTwoToneIcon className="h-4" />
+                <img className="h-4" src={language} alt="" />
                 <span className="pl-2">言語</span>
             </a>
             <a href="/learning" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <SchoolTwoToneIcon className="h-4" />
+                <img className="h-4" src={learning} alt="" />
                 <span className="pl-2">学習</span>
             </a>
             <a href="/article" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <ArticleTwoToneIcon className="h-4" />
+                <img className="h-4" src={article} alt="" />
                 <span className="pl-2">記事</span>
             </a>
             <a href="/useful" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <LabelTwoToneIcon className="h-4" />
+                <img className="h-4" src={useful} alt="" />
                 <span className="pl-2">便利</span>
             </a>
             <a href="/material" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <PanoramaTwoToneIcon className="h-4" />
+                <img className="h-4" src={material} alt="" />
                 <span className="pl-2">素材</span>
             </a>
             <a href="/other" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <MoreHorizTwoToneIcon className="h-4" />
+                <img className="h-4" src={other} alt="" />
                 <span className="pl-2">その他</span>
             </a>
         </>
