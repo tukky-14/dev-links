58a199d775c291b63989b876928d85500aed5bb5
tukky-14 <bigoonage0104@gmail.com>
2023-03-06 21:45:03 +0900

    記事ページを更新

--- src/features/article/article.ts
+++ src/features/article/article.ts
@@ -1,8 +1,80 @@
+import qiita from './img/qiita.png';
+import zenn from './img/zenn.png';
+import developersio from './img/developersio.png';
+import itmedia from './img/itmedia.png';
+import codezine from './img/codezine.png';
+import itleaders from './img/itleaders.png';
+import zdnet from './img/zdnet.png';
+import wired from './img/wired.png';
+import codegrid from './img/codegrid.png';
+import techcrunch from './img/techcrunch.png';
+
 export const articleSites = [
     {
-        title: 'xxxxxxx',
-        url: 'xxxxxxxxx',
-        image: '',
-        description: 'xxxxxxxxxxxxxxx',
+        title: 'Qiita',
+        url: 'https://qiita.com/',
+        image: qiita,
+        description:
+            'プログラミングや開発に関する技術情報を共有するコミュニティサイト。開発者が記事を投稿し、議論やフィードバックを行うことができる。',
+    },
+    {
+        title: 'Zenn',
+        url: 'https://zenn.dev/',
+        image: zenn,
+        description:
+            'プログラミング、開発に関する情報を提供するコミュニティサイト。記事の投稿の他、本を執筆することもできる。',
+    },
+    {
+        title: 'DevelopersIO',
+        url: 'https://dev.classmethod.jp/',
+        image: developersio,
+        description: 'AWSやGCPなどのクラウド技術を中心に、技術情報を提供するコミュニティサイト。',
+    },
+    {
+        title: '@IT',
+        url: 'https://atmarkit.itmedia.co.jp/',
+        image: itmedia,
+        description:
+            'プログラミングや開発、クラウド、ネットワークなど、IT関連の情報を提供するオンラインメディア。',
+    },
+    {
+        title: 'CodeZine',
+        url: 'https://codezine.jp/',
+        image: codezine,
+        description:
+            'プログラミングや開発に関する情報を提供するオンラインメディア。テクノロジーのトレンドや製品の紹介なども行う。',
+    },
+    {
+        title: 'IT Leaders',
+        url: 'https://it.impress.co.jp/',
+        image: itleaders,
+        description: 'デジタルビジネスを加速する専門情報サイト。',
+    },
+    {
+        title: 'ZDNET',
+        url: 'https://japan.zdnet.com/',
+        image: zdnet,
+        description: 'CIOの課題を解決する、米国のオンラインメディア。',
+    },
+    {
+        title: 'WIRED',
+        url: 'https://wired.jp/',
+        image: wired,
+        description:
+            '生活のあらゆる側面をテクノロジーがいかに変えていくのかに光を当てた、"未来"を実装するメディア。',
+    },
+    {
+        title: 'CodeGrid',
+        url: 'https://www.codegrid.net/',
+        image: codegrid,
+        description:
+            'フロントエンド技術向上をサポートする良質な記事を提供するサイト。30日間の無料後、有料プランの契約が必要。',
+    },
+    {
+        title: 'TechCrunch',
+        url: 'https://techcrunch.com/',
+        image: techcrunch,
+        description:
+            'IT系のスタートアップ（ベンチャー）やWebに関するニュースを配信している米国のサイト。',
     },
 ];
