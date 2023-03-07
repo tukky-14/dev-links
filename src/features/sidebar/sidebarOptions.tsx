58b306710411193fc2a26b5cafd32603ce628f1b
tukky-14 <bigoonage0104@gmail.com>
2023-03-07 21:44:12 +0900

    サイドバーをリファクタリング

--- src/features/sidebar/sidebarOptions.tsx
+++ src/features/sidebar/sidebarOptions.tsx
@@ -1,42 +1,32 @@
 import home from './img/home.svg';
-import language from './img/language.svg';
 import learning from './img/learning.svg';
 import article from './img/article.svg';
 import useful from './img/useful.svg';
 import material from './img/material.svg';
 import other from './img/other.svg';
 
+const menuList = [
+    { name: 'ホーム', link: '/', icon: home },
+    { name: '学習', link: '/learning', icon: learning },
+    { name: '記事', link: '/article', icon: article },
+    { name: '便利', link: '/useful', icon: useful },
+    { name: '素材', link: '/material', icon: material },
+    { name: 'その他', link: '/other', icon: other },
+];
+
 export const SidebarOptions = () => {
     return (
         <>
-            <a href="/" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={home} alt="" />
-                <span className="pl-2">ホーム</span>
-            </a>
-            {/* <a href="/language" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={language} alt="" />
-                <span className="pl-2">言語</span>
-            </a> */}
-            <a href="/learning" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={learning} alt="" />
-                <span className="pl-2">学習</span>
-            </a>
-            <a href="/article" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={article} alt="" />
-                <span className="pl-2">記事</span>
-            </a>
-            <a href="/useful" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={useful} alt="" />
-                <span className="pl-2">便利</span>
-            </a>
-            <a href="/material" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={material} alt="" />
-                <span className="pl-2">素材</span>
-            </a>
-            <a href="/other" className="pl-4 py-2 flex items-center hover:bg-gray-100">
-                <img className="h-4" src={other} alt="" />
-                <span className="pl-2">その他</span>
-            </a>
+            {menuList.map(({ name, link, icon }, index) => (
+                <a
+                    href={link}
+                    className="pl-4 py-2 flex items-center hover:bg-gray-100"
+                    key={index}
+                >
+                    <img className="h-4" src={icon} alt={`${name}のアイコン画像`} />
+                    <span className="pl-2">{name}</span>
+                </a>
+            ))}
         </>
     );
 };
