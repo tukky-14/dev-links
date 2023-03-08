ef638d7d0e881d5f97c0407ab38edb3d718f65e0
tukky-14 <bigoonage0104@gmail.com>
2023-03-08 21:22:50 +0900

    ハンバーガーメニューの追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
     theme: {
         extend: {
             animation: {
-                'fade-in-top': 'fade-in-top 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both',
+                'fade-in-top': 'fade-in-top 1.0s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+                'fade-in': 'fade-in 0.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
             },
             keyframes: {
                 'fade-in-top': {
@@ -14,6 +15,14 @@ module.exports = {
                     },
                     to: {
                         transform: 'translateY(0)',
+                        opacity: '1',
+                    },
+                },
+                'fade-in': {
+                    '0%': {
+                        opacity: '0',
+                    },
+                    to: {
                         opacity: '1',
                     },
                 },
