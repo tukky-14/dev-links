2cd7aba40f9113adfbe8e0bcc736bcd036c851a4
tukky-14 <bigoonage0104@gmail.com>
2023-03-21 14:54:48 +0900

    Chrome拡張機能の画像を修正

--- src/features/chrome/chrome.ts
+++ src/features/chrome/chrome.ts
@@ -1,74 +1,25 @@
-import video_speed_controller from './img/video_speed_controller.png';
-import google_translate from './img/google_translate.png';
-import notion_web_clipper from './img/notion_web_clipper.png';
-import ublacklist from './img/ublacklist.png';
-import pasty from './img/pasty.png';
-import gettabinfo from './img/gettabinfo.png';
-import wappalyzer from './img/wappalyzer.png';
-import g_calize from './img/g_calize.png';
-import blackbox from './img/blackbox.png';
 import authenticator from './img/authenticator.png';
-import keepa from './img/keepa.png';
-import dailydev from './img/dailydev.png';
-import glarity from './img/glarity.png';
+import blackbox from './img/blackbox.png';
 import crxmouse from './img/crxmouse.png';
+import dailydev from './img/dailydev.png';
+import g_calize from './img/g_calize.png';
+import gettabinfo from './img/gettabinfo.png';
+import glarity from './img/glarity.png';
+import google_translate from './img/google_translate.png';
+import keepa from './img/keepa.png';
+import notion_web_clipper from './img/notion_web_clipper.png';
+import pasty from './img/pasty.png';
+import ublacklist from './img/ublacklist.png';
+import video_speed_controller from './img/video_speed_controller.png';
+import wappalyzer from './img/wappalyzer.png';
 
 export const chromeSites = [
     {
-        title: 'Video Speed Controller',
-        url: 'https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk?hl=ja',
-        image: video_speed_controller,
+        title: 'Authenticator',
+        url: 'https://chrome.google.com/webstore/detail/authenticator/bhghoamapcdpbohphigoooaddinpkbai?hl=ja',
+        image: authenticator,
         description:
-            'ブラウザで表示されたありとあらゆる動画の再生速度をコントロール。2倍速以上も設定可能。YouYubeもUdemyもこれで再生速度を自由自在に。',
-    },
-    {
-        title: 'Google 翻訳',
-        url: 'https://chrome.google.com/webstore/detail/google-translate/aapbdbdomjkkjkaonfhkkikfgjllcleb',
-        image: google_translate,
-        description:
-            'いつも使っているGoogle翻訳も、拡張機能で操作速度UP。選択した言語を翻訳ページを開かず素早く翻訳可能。',
-    },
-    {
-        title: 'Notion Web Clipper',
-        url: 'https://chrome.google.com/webstore/detail/notion-web-clipper/knheggckgoiihginacbkhaalnibhilkk?hl=ja',
-        image: notion_web_clipper,
-        description:
-            '現在のページを指定したNotionのページに保存。リンクだけでなく、ページの内容もすべて保存する、Notionを使用しているならマストで入れる拡張機能。',
-    },
-    {
-        title: 'uBlacklist',
-        url: 'https://chrome.google.com/webstore/detail/ublacklist/pncfbmialoiaghdehhbnbhkkgmjanfhe?hl=ja',
-        image: ublacklist,
-        description:
-            '表示したくないサイトを検索結果からブロック。SEO対策だけの粗悪な記事を除去可能。',
-    },
-    {
-        title: 'Pasty',
-        url: 'https://chrome.google.com/webstore/detail/pasty/hdjihnnclpjhfdbbinmgoiehhoehhlgf',
-        image: pasty,
-        description:
-            'クリップボードに保存したURLをまとめてタブに表示できる。GetTabInfoとコンビで使うとなおよし。',
-    },
-    {
-        title: 'GetTabInfo',
-        url: 'https://chrome.google.com/webstore/detail/gettabinfo/iadhcoaabobddcebhmheikmbcjcigjhc',
-        image: gettabinfo,
-        description:
-            'タブに表示しているリンクをクリップボードにまとめて保存。保存の仕方もテンプレートで指定可能。',
-    },
-    {
-        title: 'Wappalyzer',
-        url: 'https://chrome.google.com/webstore/detail/wappalyzer-technology-pro/gppongmhjkpfnbhagpmjfkannfbllamg?hl=ja',
-        image: wappalyzer,
-        description:
-            '開いているページに使用されている言語やフレームワークを表示。気になるサイトが何の技術で作成されているかチェックするのは楽しい。',
-    },
-    {
-        title: 'G-calize',
-        url: 'https://chrome.google.com/webstore/detail/g-calize/piiljfhidimfponnkjlkecnpjhdijfde?hl=ja',
-        image: g_calize,
-        description:
-            'Googleカレンダーを色付けできる。土日に色をつけるとわかりやすい。痒いところに手が届く拡張機能。',
+            'AWS MFAなどの多要素認証を拡張機能を使用して登録できる。セキュリティはある程度維持したいが利便性は失いたくない時に。',
     },
     {
         title: 'Blackbox',
@@ -78,18 +29,11 @@ export const chromeSites = [
             'ブラウザで表示させた画像やPDF内の文章をテキスト化できる拡張機能。画像や動画内のコードを手打ちではなく、スクショしてテキスト貼り付けできる画期的機能。',
     },
     {
-        title: 'Authenticator',
-        url: 'https://chrome.google.com/webstore/detail/authenticator/bhghoamapcdpbohphigoooaddinpkbai?hl=ja',
-        image: authenticator,
+        title: 'crxMouse',
+        url: 'https://chrome.google.com/webstore/detail/crxmouse-chrome-gestures/jlgkpaicikihijadgifklkbpdajbkhjo',
+        image: crxmouse,
         description:
-            'AWS MFAなどの多要素認証を拡張機能を使用して登録できる。セキュリティはある程度維持したいが利便性は失いたくない時に。',
-    },
-    {
-        title: 'Keepa',
-        url: 'https://chrome.google.com/webstore/detail/keepa-amazon-price-tracke/neebplgakaahbhdphmkckjjcegoiijjo?hl=ja',
-        image: keepa,
-        description:
-            'Amazonに出品されている商品の価格変動や売れ筋ランキング、レビュー数などがわかる拡張機能。賢く買い物しよう。',
+            'マウスの動きにさまざまな操作を割り当てることができる。片手だけでいろんな操作をコントロール。',
     },
     {
         title: 'daily.dev',
@@ -99,6 +43,20 @@ export const chromeSites = [
             'インストールするとGoogleChromeのホーム画面が海外のTechメディアのRSSを表示する画面になる。最新技術の情報収集が捗る。',
     },
     {
+        title: 'G-calize',
+        url: 'https://chrome.google.com/webstore/detail/g-calize/piiljfhidimfponnkjlkecnpjhdijfde?hl=ja',
+        image: g_calize,
+        description:
+            'Googleカレンダーを色付けできる。土日に色をつけるとわかりやすい。痒いところに手が届く拡張機能。',
+    },
+    {
+        title: 'GetTabInfo',
+        url: 'https://chrome.google.com/webstore/detail/gettabinfo/iadhcoaabobddcebhmheikmbcjcigjhc',
+        image: gettabinfo,
+        description:
+            'タブに表示しているリンクをクリップボードにまとめて保存。保存の仕方もテンプレートで指定可能。',
+    },
+    {
         title: 'glarity',
         url: 'https://chrome.google.com/webstore/detail/glarity-summary-for-googl/cmnlolelipjlhfkhpohphpedmkfbobjc',
         image: glarity,
@@ -106,10 +64,52 @@ export const chromeSites = [
             'ChatGPTを使用し、Google検索やYouTube動画などのWebサイトコンテンツの要約を生成する拡張機能。',
     },
     {
-        title: 'crxMouse',
-        url: 'https://chrome.google.com/webstore/detail/glarity-summary-for-googl/cmnlolelipjlhfkhpohphpedmkfbobjc',
-        image: crxmouse,
+        title: 'Google 翻訳',
+        url: 'https://chrome.google.com/webstore/detail/google-translate/aapbdbdomjkkjkaonfhkkikfgjllcleb',
+        image: google_translate,
         description:
-            'マウスの動きにさまざまな操作を割り当てることができる。片手だけでいろんな操作をコントロール。',
+            'いつも使っているGoogle翻訳も、拡張機能で操作速度UP。選択した言語を翻訳ページを開かず素早く翻訳可能。',
+    },
+    {
+        title: 'Keepa',
+        url: 'https://chrome.google.com/webstore/detail/keepa-amazon-price-tracke/neebplgakaahbhdphmkckjjcegoiijjo?hl=ja',
+        image: keepa,
+        description:
+            'Amazonに出品されている商品の価格変動や売れ筋ランキング、レビュー数などがわかる拡張機能。賢く買い物しよう。',
+    },
+    {
+        title: 'Notion Web Clipper',
+        url: 'https://chrome.google.com/webstore/detail/notion-web-clipper/knheggckgoiihginacbkhaalnibhilkk?hl=ja',
+        image: notion_web_clipper,
+        description:
+            '現在のページを指定したNotionのページに保存。リンクだけでなく、ページの内容もすべて保存する、Notionを使用しているならマストで入れる拡張機能。',
+    },
+    {
+        title: 'Pasty',
+        url: 'https://chrome.google.com/webstore/detail/pasty/hdjihnnclpjhfdbbinmgoiehhoehhlgf',
+        image: pasty,
+        description:
+            'クリップボードに保存したURLをまとめてタブに表示できる。GetTabInfoとコンビで使うとなおよし。',
+    },
+    {
+        title: 'uBlacklist',
+        url: 'https://chrome.google.com/webstore/detail/ublacklist/pncfbmialoiaghdehhbnbhkkgmjanfhe?hl=ja',
+        image: ublacklist,
+        description:
+            '表示したくないサイトを検索結果からブロック。SEO対策だけの粗悪な記事を除去可能。',
+    },
+    {
+        title: 'Video Speed Controller',
+        url: 'https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk?hl=ja',
+        image: video_speed_controller,
+        description:
+            'ブラウザで表示されたありとあらゆる動画の再生速度をコントロール。2倍速以上も設定可能。YouYubeもUdemyもこれで再生速度を自由自在に。',
+    },
+    {
+        title: 'Wappalyzer',
+        url: 'https://chrome.google.com/webstore/detail/wappalyzer-technology-pro/gppongmhjkpfnbhagpmjfkannfbllamg?hl=ja',
+        image: wappalyzer,
+        description:
+            '開いているページに使用されている言語やフレームワークを表示。気になるサイトが何の技術で作成されているかチェックするのは楽しい。',
     },
 ];
