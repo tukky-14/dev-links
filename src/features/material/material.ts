909e2de306d65d021596589976868a9e39c07825
tukky-14 <bigoonage0104@gmail.com>
2023-03-30 21:40:05 +0900

    素材のイラストサイトを追加

--- src/features/material/material.ts
+++ src/features/material/material.ts
@@ -1,12 +1,16 @@
 import odan from './img/odan.png';
-import tyoudoii from './img/tyoudoii.png';
 import flaticon from './img/flaticon.png';
 import icooonmono from './img/icooonmono.png';
-import pakutaso from './img/pakutaso.png';
-import irasutoya from './img/irasutoya.png';
 import storyset from './img/storyset.png';
 import devicon from './img/devicon.png';
 import googlefonts from './img/googlefonts.png';
+import pakutaso from './img/pakutaso.png';
+import irasutoya from './img/irasutoya.png';
+import tyoudoii from './img/tyoudoii.png';
+import loosedrawing from './img/loosedrawing.png';
+import illust_navi from './img/illust_navi.png';
+import linustock from './img/linustock.png';
+import vectorshelf from './img/vectorshelf.png';
 
 export const materialSites = [
     {
@@ -15,13 +19,6 @@ export const materialSites = [
         image: odan,
         description:
             '商用利用可能なフリー素材を提供する、写真素材のO-DAN(横断)検索できるサイト。日本の風景や食べ物などを中心に、多数の素材が揃っている。',
-    },
-    {
-        title: 'ちょうどいいイラスト',
-        url: 'https://tyoudoii-illust.com/',
-        image: tyoudoii,
-        description:
-            '商用利用可能なイラスト素材を提供するサイト。会社組織図やプレゼン資料の挿絵などに利用できる。',
     },
     {
         title: 'flaticon',
@@ -36,20 +33,6 @@ export const materialSites = [
         image: icooonmono,
         description:
             '商用利用可能なモノクロアイコン素材を提供するサイト。シンプルなデザインで、多くのカテゴリーに対応している。',
-    },
-    {
-        title: 'PAKUTASO',
-        url: 'https://www.pakutaso.com/',
-        image: pakutaso,
-        description:
-            '商用利用可能なフリー写真素材を提供するサイト。ライフスタイルやビジネスなど、多くのジャンルの写真が揃っている。',
-    },
-    {
-        title: 'いらすとや',
-        url: 'https://www.irasutoya.com/',
-        image: irasutoya,
-        description:
-            '商用利用可能なイラスト素材を提供するサイト。多彩なシチュエーションのイラストが揃っており、ビジネスや教育などに利用できる。',
     },
     {
         title: 'storyset',
@@ -71,5 +54,49 @@ export const materialSites = [
         image: googlefonts,
         description:
             '「Open Source Fonts」で商用・非商用に関わらず誰でも無料で使用することができる Web フォントを提供するGoogleのサービス。',
+    },
+    {
+        title: 'PAKUTASO',
+        url: 'https://www.pakutaso.com/',
+        image: pakutaso,
+        description:
+            '商用利用可能なフリー写真素材を提供するサイト。ライフスタイルやビジネスなど、多くのジャンルの写真が揃っている。',
+    },
+    {
+        title: 'いらすとや',
+        url: 'https://www.irasutoya.com/',
+        image: irasutoya,
+        description:
+            '商用利用可能なイラスト素材を提供するサイト。多彩なシチュエーションのイラストが揃っており、ビジネスや教育などに利用できる。',
+    },
+    {
+        title: 'ちょうどいいイラスト',
+        url: 'https://tyoudoii-illust.com/',
+        image: tyoudoii,
+        description: '商用利用可能なイラスト素材を提供するサイト。',
+    },
+    {
+        title: 'Loose Drawing',
+        url: 'https://loosedrawing.com/',
+        image: loosedrawing,
+        description: '商用利用可能なイラスト素材を提供するサイト。',
+    },
+    {
+        title: 'ILLUST NAVI',
+        url: 'https://illust-navi.com/',
+        image: illust_navi,
+        description: '商用利用可能なイラスト素材を提供するサイト。',
+    },
+    {
+        title: 'Linustock',
+        url: 'https://www.linustock.com/vectors',
+        image: linustock,
+        description: '商用利用可能なイラスト素材を提供するサイト。',
+    },
+    {
+        title: 'vectorShelf',
+        url: 'https://vectorshelf.com/',
+        image: vectorshelf,
+        description: '商用利用可能なイラスト素材を提供するサイト。',
     },
 ];
